355bbdfb5f65c1305fcd9dcd7915bbac542a4621
YB <109190196+yoojin93@users.noreply.github.com>
2023-01-29 18:40:53 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+def fetchWeather(city):
+  URL = "http://api.openweathermap.org/data/2.5/weather?"
+  PARAMS = {"appid": "c8efac34cc3548754ca009222d24da49", "q": city, "units": "imperial"}
+  response = requests.get(url = URL, params = PARAMS).json()
+  temperature = response["main"]["temp"]
+  print(temperature)
+  if temperature <= 65:
+    print("You should wear a coat!")
+  if temperature > 65:
+    print("You are safe to wear shorts!")
+
+userCity = input("Enter a city:")
+fetchWeather(userCity)
